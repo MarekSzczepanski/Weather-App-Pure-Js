14e9a4beb274540ecf8affe48904ef6dbe968e34
Marek Szczepanski <marek.szczepanski1@onet.pl>
2019-10-17 18:14:57 +0200

    Full layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log("test");
 let log;
 let icon;
-const url = "https://api.openweathermap.org/data/2.5/weather?q=los angeles&APPID=ee37e5abb8ef2c3cf384075cf513d7e0";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=new york&APPID=ee37e5abb8ef2c3cf384075cf513d7e0";
 fetch(url)
 .then(response => {
     return response.json()
@@ -9,12 +9,9 @@ fetch(url)
 .then(
     json => {
         document.querySelector("img").src = `http://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`,
-        json.main.temp, 
-        json.wind.speed, 
-        json.main.pressure
+        document.querySelector(".temperature-value").textContent = Math.floor(json.main.temp - 273.15) + "°C", 
+        document.querySelector(".humidity-value").textContent = json.main.humidity + "%"
+        document.querySelector(".wind-value").textContent = json.wind.speed + "m/s", 
+        console.log(json)
     }, 
 )
-.then(
-    
-)
-

fad56bb1084dba562031b5cc1abbf219f223a02c
Marek Szczepanski <marek.szczepanski1@onet.pl>
2019-10-18 20:00:22 +0200

    Add functions

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
-console.log("test");
-let log;
-let icon;
-const url = "https://api.openweathermap.org/data/2.5/weather?q=new york&APPID=ee37e5abb8ef2c3cf384075cf513d7e0";
-fetch(url)
-.then(response => {
-    return response.json()
-})
-.then(
-    json => {
-        document.querySelector("img").src = `https://openweathermap.org/img/wn/${json.weather[0].icon}@2x.png`,
-        document.querySelector(".temperature-value").textContent = Math.floor(json.main.temp - 273.15) + "°C", 
-        document.querySelector(".humidity-value").textContent = json.main.humidity + "%"
-        document.querySelector(".wind-value").textContent = /* json.wind.speed */ 88.8 + " m/s", 
-        console.log(json)
-    }, 
-)
+let city;
+let url;
+const getWeather = () => {
+    return fetch(url)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => err)
+}
+
+const showWeather = () => {
+    city = document.querySelector(".form-control").value;
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ee37e5abb8ef2c3cf384075cf513d7e0`;
+    document.querySelector(".info").style.display = "block";
+    document.querySelector(".info").scrollIntoView({ 
+        behavior: 'smooth'
+    });
+    getWeather().then((data) => {
+        document.querySelector("img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.querySelector(".temperature-value").textContent = Math.floor(data.main.temp - 273.15) + "°C"
+        document.querySelector(".humidity-value").textContent = data.main.humidity + "%"
+        document.querySelector(".wind-value").textContent = data.wind.speed + " m/s" 
+        console.log(data)
+    })
+}
+
+document.querySelector(".btn").addEventListener("click", showWeather);
+document.querySelector(".form-control").addEventListener("keydown", (e) => {
+    if (e.keyCode == 13) {
+        showWeather();
+    }
+});
+
+
